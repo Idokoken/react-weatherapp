ea71e8afe0da86b8fba02bbea282a771147c9c7b
unknown <idokokennethnduka@gmail.com>
2021-11-23 03:53:38 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+//import axios from "axios";
+import moment from "moment";
 
 function App() {
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("lagos");
+  const [search, setSearch] = useState("");
+
+  const d = new Date();
+  const newD = moment(d).format("h:mm a, MMM Do YYYY");
+
+  const changeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const fetchDetail = async () => {
+    const api1 =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      query +
+      "&units=metric&appid=eae4c7df9d0c80084e5da9ace7f1a8b9";
+    try {
+      const resp = await fetch(api1);
+      const data = await resp.json();
+      setWeather(data);
+      //setQuery("");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // const fetchDetail = async () => {
+  //   const api =
+  //     "https://api.openweathermap.org/data/2.5/weather?q=Enugu,NG&appid=eae4c7df9d0c80084e5da9ace7f1a8b9";
+  //   try {
+  //     const response = await axios.get(api);
+  //     setWeather(response.data);
+  //     setQuery("");
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    fetchDetail();
+  }, [query]);
+
+  const handleSumit = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather">
+      <div id="weather-app">
+        <div className="app-name">CREATIVE KEN</div>
+        <div className="app-name">Weather App</div>
+      </div>
+      <div className="details">
+        <div className="weather-search">
+          <form onSubmit={handleSumit}>
+            <input type="text" value={search} onChange={changeHandler} />
+            <button className="btn btn-primary">Search</button>
+          </form>
+        </div>
+
+        <div id="weather-details">
+          <div className="location-date">{newD}</div>
+          <div className="location-city">
+            {weather ? weather.name + ", " : ""}
+            {weather.sys ? weather.sys.country : ""}
+          </div>
+          <div className="temp-value">
+            {weather.main ? weather.main.temp : ""}
+          </div>
+          <div className="weather-icon">
+            <img
+              src={
+                weather.weather
+                  ? "http://openweathermap.org/img/w/" +
+                    weather.weather[0].icon +
+                    ".png"
+                  : ""
+              }
+              alt="weather icon"
+            />
+          </div>
+          <div className="temp-reading">
+            {weather.weather ? weather.weather[0].description : ""}
+          </div>
+        </div>
+      </div>
+      <footer>
+        <p>&copy; 2021. All Right Reserved to Idoko Kenneth</p>
+      </footer>
     </div>
   );
 }
